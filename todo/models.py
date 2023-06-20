a22383b88f3dfb0d5cb4e622c2818a8a09b76936
thibedi-phathela <3932776@myuwc.ac.za>
2023-06-20 17:59:57 +0200

    Intial commit

---
+++ todo/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from datetime import date
+
+class Todo(models.Model):
+    title = models.CharField(max_length=1000)
+    date = models.CharField(max_length=15,default=date.today)
+
+    def __str__(self):
+        return f"{self.title} - {self.date}"
